0c64af0a7173a6e28954ff410d4a5e80ee54e393
natdagr85 <nathanmansoor1128@gmail.com>
2022-07-19 22:05:45 -0700

    edited instruction button

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const openInst = document.querySelector(".open-instructions");
 const hidden = document.querySelector(".hidden");
 const closeInst = document.querySelector(".close-instructions");
+const blur = document.querySelector(".overlay");
 
 openInst.addEventListener("click", function instructions() {
   hidden.classList.remove("hidden");
+  blur.classList.remove("hidden");
 });
 
 closeInst.addEventListener("click", function instructions() {
   hidden.classList.add("hidden");
+  blur.classList.add("hidden");
 });
