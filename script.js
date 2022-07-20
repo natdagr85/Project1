08ebf1c3253ec86c571864ee0bff43cf3ea635ee
natdagr85 <nathanmansoor1128@gmail.com>
2022-07-19 21:30:44 -0700

    added more script and styling

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const openInst = document.querySelector(".open-instructions");
 const hidden = document.querySelector(".hidden");
+const closeInst = document.querySelector(".close-instructions");
 
 openInst.addEventListener("click", function instructions() {
   hidden.classList.remove("hidden");
+});
+
+closeInst.addEventListener("click", function instructions() {
+  hidden.classList.add("hidden");
 });
