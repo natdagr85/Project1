759fa2f3878a881562942b15f3fdaca945ee8942
natdagr85 <nathanmansoor1128@gmail.com>
2022-07-24 11:13:02 -0700

    adjusted some stylind and changed up layout slightly

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const openInst = document.querySelector(".open-instructions");
 const hidden = document.querySelector(".hidden");
 const closeInst = document.querySelector(".close-instructions");
 const blur = document.querySelector(".overlay");
+const quiz = document.querySelector(".quiz");
+const start = document.querySelector(".start-btn");
+const submit = document.querySelector(".submit");
+
+const answers = [false, true, false, true, true];
 
 openInst.addEventListener("click", function instructions() {
   hidden.classList.remove("hidden");
@@ -11,4 +16,11 @@ openInst.addEventListener("click", function instructions() {
 closeInst.addEventListener("click", function instructions() {
   hidden.classList.add("hidden");
   blur.classList.add("hidden");
+});
+
+start.addEventListener("click", function start() {
+  quiz.classList.remove("hidden");
+  submit.classList.remove("hidden");
+  openInst.classList.add("hidden");
+  start.classList.add("hidden");
 });
