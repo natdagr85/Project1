4ba75377e12fede1a64856512d54fcf615aedf12
natdagr85 <nathanmansoor1128@gmail.com>
2022-07-25 12:58:53 -0700

    added script for making trivia work and more styling

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ const blur = document.querySelector(".overlay");
 const quiz = document.querySelector(".quiz");
 const start = document.querySelector(".start-btn");
 const submit = document.querySelector(".submit");
+const scoreOverlay = document.querySelector(".score-overlay");
+const scoreP = document.querySelector(".score-p");
 
 const answers = [false, true, false, true, true];
+let input = [];
+let score = 0;
 
 openInst.addEventListener("click", function instructions() {
   hidden.classList.remove("hidden");
@@ -22,5 +26,61 @@ start.addEventListener("click", function start() {
   quiz.classList.remove("hidden");
   submit.classList.remove("hidden");
   openInst.classList.add("hidden");
-  start.classList.add("hidden");
+});
+
+document.querySelector(".q1t").addEventListener("click", function truebtn() {
+  input.push(true);
+});
+
+document.querySelector(".q2t").addEventListener("click", function truebtn() {
+  input.push(true);
+});
+
+document.querySelector(".q3t").addEventListener("click", function truebtn() {
+  input.push(true);
+});
+
+document.querySelector(".q4t").addEventListener("click", function truebtn() {
+  input.push(true);
+});
+
+document.querySelector(".q5t").addEventListener("click", function truebtn() {
+  input.push(true);
+});
+
+document.querySelector(".q1f").addEventListener("click", function truebtn() {
+  input.push(false);
+});
+
+document.querySelector(".q2f").addEventListener("click", function truebtn() {
+  input.push(false);
+});
+
+document.querySelector(".q3f").addEventListener("click", function truebtn() {
+  input.push(false);
+});
+
+document.querySelector(".q4f").addEventListener("click", function truebtn() {
+  input.push(false);
+});
+
+document.querySelector(".q5f").addEventListener("click", function truebtn() {
+  input.push(false);
+});
+
+submit.addEventListener("click", function finish() {
+  if (answers[0] == input[0]) {
+    score += 20;
+  } else if (answers[1] == input[1]) {
+    score += 20;
+  } else if (answers[2] == input[2]) {
+    score += 20;
+  } else if (answers[3] == input[3]) {
+    score += 20;
+  } else if (answers[4] == input[4]) {
+    score += 20;
+  }
+
+  blur.classList.remove("hidden");
+  scoreOverlay.classList.remove("hidden");
 });
