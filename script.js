dbbc39332061e167d28e937437215cfa47276cbc
Nathan Mansoor <nathanmansoor1128@gmail.com>
2022-07-26 07:46:43 -0700

    added comments in script

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//created variables for DOM elements to make scripting easier
 const openInst = document.querySelector(".open-instructions");
 const hidden = document.querySelector(".hidden");
 const closeInst = document.querySelector(".close-instructions");
@@ -9,10 +10,12 @@ const scoreOverlay = document.querySelector(".score-overlay");
 const scoreP = document.querySelector(".score-p");
 const restart = document.querySelector(".rst-btn");
 
+//answers are the correct answers to trivia, input is the users input
 const answers = [false, true, false, true, true];
 let input = [];
 let score = 0;
 
+//adding scripting to buttons to remove/add hidden class tag
 openInst.addEventListener("click", function instructions() {
   hidden.classList.remove("hidden");
   blur.classList.remove("hidden");
@@ -29,6 +32,7 @@ start.addEventListener("click", function start() {
   openInst.classList.add("hidden");
 });
 
+//true or false buttons for quiz push user answer to input array
 document.querySelector(".q1t").addEventListener("click", function truebtn() {
   input.push(true);
 });
@@ -69,6 +73,8 @@ document.querySelector(".q5f").addEventListener("click", function truebtn() {
   input.push(false);
 });
 
+//submit button checks if answer and input values are the same, if so add 20%.
+//submit button also calculates score and pushes through dom to show user
 submit.addEventListener("click", function finish() {
   if (answers[0] == input[0]) {
     score += 20;
@@ -90,6 +96,7 @@ submit.addEventListener("click", function finish() {
   scoreOverlay.classList.remove("hidden");
 });
 
+//restart button reverts all display and input/score back to original so user can play again
 restart.addEventListener("click", function rst() {
   scoreOverlay.classList.add("hidden");
   blur.classList.add("hidden");
